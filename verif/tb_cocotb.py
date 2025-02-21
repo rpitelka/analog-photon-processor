3e0022a5c502c47200162a5355df005c7630888b
anikolica <13853168+anikolica@users.noreply.github.com>
2025-02-21 11:43:49 -0500

    First working (very simple) cocotb test

--- verif/tb_cocotb.py
+++ verif/tb_cocotb.py
@@ -10,10 +10,7 @@ from cocotb.result import TestSuccess, TestFailure, ReturnValue
 from cocotb.utils import get_sim_time
 
 @cocotb.test()
-
-async def my_first_test(tb):
-    """Try accessing the design."""
-
-    for cycle in range(10):
-        await Timer(1000, units="ns")
+def my_first_test(tb):
+	"""Try accessing the design."""
+	yield Timer(1000, units="ns")
 
